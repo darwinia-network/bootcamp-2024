791dcf5bffd4ea15d6eabcb6c91f500cfabb0b2d
bear <boundless.forest@outlook.com>
2024-09-04 17:28:40 +0800

    Add retry

--- erc721/scripts/play.js
+++ erc721/scripts/play.js
@@ -5,72 +5,90 @@ const { generateTokenUrl } = require("./token_url");
 
 // Get free token from the faucet: https://faucet.triangleplatform.com/darwinia/koi
 const tokenOwner = {
-    address: "0x6Bc9543094D17f52CF6b419FB692797E48d275d0",
-    privateKey: "0xd5cef12c5641455ad949c3ce8f9056478eeda53dcbade335b06467e8d6b2accc",
-}
+	address: "0x6Bc9543094D17f52CF6b419FB692797E48d275d0",
+	privateKey:
+		"0xd5cef12c5641455ad949c3ce8f9056478eeda53dcbade335b06467e8d6b2accc",
+};
 
 const network = new ethers.Network("koi", 701);
-const provider = new ethers.JsonRpcProvider('https://koi-rpc.darwinia.network', network, {
-    staticNetwork: network
-});
+const provider = new ethers.JsonRpcProvider(
+	"https://koi-rpc.darwinia.network",
+	network,
+	{
+		staticNetwork: network,
+	}
+);
 const wallet = new ethers.Wallet(tokenOwner.privateKey, provider);
 const abi = contractMetadata.contracts["BootcampERC721.sol:Bootcamp"].abi;
 
 const testing = async () => {
-    const contractAddress = await fs.readFileSync("./scripts/token-address.txt", "utf8");
-    // Construct the contract instance
-    const contract = new ethers.Contract(contractAddress, abi, wallet);
+	const contractAddress = await fs.readFileSync(
+		"./scripts/token-address.txt",
+		"utf8"
+	);
+	// Construct the contract instance
+	const contract = new ethers.Contract(contractAddress, abi, wallet);
 
-    // Mint the token
-    let nextTokenId = await contract.nextTokenId();
-    console.log("Mint the NFT token: id = ", Number(nextTokenId));
-    // let token = await generateTokenUrl("LA", nextTokenId);
-    let token = "ipfs://bafkreidpx2sbcgvwdxxtjm2pz5vjkq2bj2qw5dtztrrqtx5eakho6t2tqq";
-    console.log("   > The token url: ", token);
-    let mint = await contract.mint(tokenOwner.address, token);
-    await mint.wait();
-    console.log("   > Token Name: ", await contract.name());
-    console.log("   > Token Symbol: ", await contract.symbol());
-    console.log("   > Token url: ", await contract.tokenURI(nextTokenId));
-    console.log("   > Token Owner: ", await contract.ownerOf(nextTokenId));
-    console.log("   > BalanceOf the token owner: ", Number(await contract.balanceOf(tokenOwner.address)));
+	const text = "TESTA";
 
-    // Transfer the token
-    let toAccount = ethers.Wallet.createRandom();
-    let toAccountAddr = toAccount.address;
-    let toAccountPrivateKey = toAccount.privateKey;
-    // Ensure the approve account has enough balance.
-    let tx = await wallet.sendTransaction({
-        to: toAccountAddr,
-        value: ethers.parseUnits("1")
-    });
-    await tx.wait();
-    console.log("Transfer the token from %s to another account %s", tokenOwner.address, toAccountAddr);
-    console.log("   > Before transfer: The owner of the token %s is %s", Number(nextTokenId), await contract.ownerOf(nextTokenId));
-    let transfer = await contract.safeTransferFrom(tokenOwner.address, toAccountAddr, nextTokenId);
-    await transfer.wait();
-    console.log("   > After Transfer: The owner of the token %s is %s", Number(nextTokenId), await contract.ownerOf(nextTokenId));
+	// Mint the token
+	let nextTokenId = await contract.nextTokenId();
+	console.log(`Mint new token: id ${nextTokenId}`);
+	let token = await generateTokenUrl(text, nextTokenId);
+	console.log(`   > The token metadata has been uploaded to IPFS, the url: ${token}`);
+	let mint = await contract.mint(tokenOwner.address, token);
+	await mint.wait();
+	console.log(`   > Token Name: ${await contract.name()}`);
+	console.log(`   > Token Symbol: ${await contract.symbol()}`);
+	console.log(`   > Token url: ${await contract.tokenURI(nextTokenId)}`);
+	console.log(`   > Token Owner: ${await contract.ownerOf(nextTokenId)}`);
+	console.log(`   > BalanceOf: `, Number(await contract.balanceOf(tokenOwner.address)));
 
-    // Approve the token
-    console.log("Approve the token to account: %s", toAccountAddr);
-    nextTokenId = await contract.nextTokenId();
-    mint = await contract.mint(tokenOwner.address, token);
-    await mint.wait();
-    console.log("   > Token owner successfully mint another token: id = ", Number(nextTokenId));
-    let approve = await contract.approve(toAccountAddr, nextTokenId);
-    await approve.wait();
-    console.log("   > The approved address is : ", await contract.getApproved(nextTokenId));
+	// Transfer the token
+	let toAccount = ethers.Wallet.createRandom();
+	let toAccountAddr = toAccount.address;
+	let toAccountPrivateKey = toAccount.privateKey;
+	// Ensure the approve account has enough balance.
+	let tx = await wallet.sendTransaction({
+		to: toAccountAddr,
+		value: ethers.parseUnits("1"),
+	});
+	await tx.wait();
+	console.log(`Transfer the token from ${tokenOwner.address} to another account ${toAccountAddr}`);
+	console.log(`   > Before transfer: The owner of the token ${Number(nextTokenId)} is ${await contract.ownerOf(nextTokenId)}`);
+	let transfer = await contract.safeTransferFrom(
+		tokenOwner.address,
+		toAccountAddr,
+		nextTokenId
+	);
+	await transfer.wait();
+	console.log(`   > After Transfer: The owner of the token ${Number(nextTokenId)} is ${await contract.ownerOf(nextTokenId)}`);
 
-    // TransferFrom the approved address
-    let newWallet = new ethers.Wallet(toAccountPrivateKey, provider);
-    let contract2 = contract.connect(newWallet);
-    let toAccount2 = ethers.Wallet.createRandom();
-    let toAccountAddr2 = toAccount2.address;
-    console.log("TransferFrom the token from %s to another account %s by %s", tokenOwner.address, toAccountAddr2, toAccountAddr);
-    console.log("   > Before transfer: The owner of the token %s is %s", Number(nextTokenId), await contract2.ownerOf(nextTokenId));
-    let transferFrom = await contract2.safeTransferFrom(tokenOwner.address, toAccount2, nextTokenId);
-    await transferFrom.wait();
-    console.log("   > After transfer: The owner of the token %s is %s", Number(nextTokenId), await contract2.ownerOf(nextTokenId));
+	// Approve the token
+	console.log(`Approve the token to account: ${toAccountAddr}`);
+	nextTokenId = await contract.nextTokenId();
+	token = await generateTokenUrl(text, nextTokenId);
+    console.log(`   > The token metadata has been uploaded to IPFS, the url: ${token}`);
+	mint = await contract.mint(tokenOwner.address, token);
+	await mint.wait();
+	console.log(`   > Token owner successfully mint another token ${Number(nextTokenId)}`);
+	let approve = await contract.approve(toAccountAddr, nextTokenId);
+	await approve.wait();
+	console.log(`   > The approved address is : ${await contract.getApproved(nextTokenId)}`);
 
-}
+	// TransferFrom the approved address
+	let newWallet = new ethers.Wallet(toAccountPrivateKey, provider);
+	let contract2 = contract.connect(newWallet);
+	let toAccount2 = ethers.Wallet.createRandom();
+	let toAccountAddr2 = toAccount2.address;
+	console.log(`TransferFrom the token from ${tokenOwner.address} to another account ${toAccountAddr2} by ${toAccountAddr}`);
+	console.log(`   > Before transfer: The owner of the token ${Number(nextTokenId)} is ${await contract2.ownerOf(nextTokenId)}`);
+	let transferFrom = await contract2.safeTransferFrom(
+		tokenOwner.address,
+		toAccount2,
+		nextTokenId
+	);
+	await transferFrom.wait();
+	console.log(`   > After transfer: The owner of the token ${Number(nextTokenId)} is ${await contract2.ownerOf(nextTokenId)}`);
+};
 testing().catch(console.error);
--- erc721/scripts/token_url.js
+++ erc721/scripts/token_url.js
@@ -5,59 +5,76 @@ const { PinataSDK } = require("pinata");
 const { Blob } = require("buffer");
 
 require("dotenv").config();
+const pinata = new PinataSDK({
+    pinataJwt: process.env.PINATA_JWT,
+    pinataGateway: process.env.GATEWAY_URL,
+});
 
 function createImage(text, width = 500, height = 500) {
-  const canvas = createCanvas(width, height);
-  const context = canvas.getContext("2d");
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext("2d");
 
-  // Draw a background
-  context.fillStyle = "#ffffff";
-  context.fillRect(0, 0, width, height);
+    // Draw a background
+    context.fillStyle = "#ffffff";
+    context.fillRect(0, 0, width, height);
 
-  // Draw some text
-  context.font = "bold 30pt Menlo";
-  context.textAlign = "center";
-  context.fillStyle = "#000000";
-  context.fillText(text, width / 2, height / 2);
+    // Draw some text
+    context.font = "bold 30pt Menlo";
+    context.textAlign = "center";
+    context.fillStyle = "#000000";
+    context.fillText(text, width / 2, height / 2);
 
-  return canvas.toBuffer("image/png");
+    return canvas.toBuffer("image/png");
 }
 
 async function generateTokenUrl(text, tokenId) {
-  const pinata = new PinataSDK({
-    pinataJwt: process.env.PINATA_JWT,
-    pinataGateway: process.env.GATEWAY_URL,
-  });
+    // Generate token image and save it to disk
+    let tokenName = text.concat(tokenId.toString());
+    let tokenPath = "./tokens/" + tokenName + ".png";
+    let imageBuffer = createImage(tokenName);
+    fs.writeFileSync(tokenPath, imageBuffer);
 
-  // Generate token image and save it to disk
-  let tokenName = text.concat(tokenId.toString());
-  let tokenPath = "./tokens/" + tokenName + ".png";
-  let imageBuffer = createImage(tokenName);
-  fs.writeFileSync(tokenPath, imageBuffer);
+    // Upload image to Pinata
+    let image = fs.readFileSync(tokenPath);
+    let blob = new Blob([image]);
+    let file = new File([blob], tokenName, { type: "image/png" });
+    // Upload image to Pinata
+    let imageUploadResponse = await retryUpload(() => pinata.upload.file(file));
 
-  // Upload image to Pinata
-  let image = fs.readFileSync(tokenPath);
-  let blob = new Blob([image]);
-  let file = new File([blob], tokenName, { type: "image/png" });
-  let imageUploadResponse = await pinata.upload.file(file);
-  console.log("imageUploadResponse: ", imageUploadResponse);
+    // Create metadata and save it to disk
+    let metadata = {
+        name: "Lesson " + tokenId + " NFT",
+        description: "This is the Darwinia Bootcamp Lesson " + tokenId + " NFT",
+        image: "ipfs://" + imageUploadResponse.cid,
+        external_url: "https://pinata.cloud",
+    };
+    let metadataPath = "./tokens/" + tokenName + ".json";
+    fs.writeFileSync(metadataPath, JSON.stringify(metadata, null, 2));
+    // Upload metadata to Pinata
+    let metadataUploadResponse = await retryUpload(() =>
+        pinata.upload.json(metadata)
+    );
 
-  // Create metadata and save it to disk
-  let metadata = {
-    name: "Lesson " + tokenId + "NFT",
-    description: "This is the Darwinia Bootcamp Lesson " + tokenId + " NFT",
-    image: "ipfs://" + imageUploadResponse.cid,
-    external_url: "https://pinata.cloud",
-  };
-  let metadataPath = "./tokens/" + tokenName + ".json";
-  fs.writeFileSync(metadataPath, JSON.stringify(metadata, null, 2));
+    // Return token metadata URL
+    return "ipfs://" + metadataUploadResponse.cid;
+}
 
-  // Upload metadata to Pinata
-  let metadataUploadResponse = await pinata.upload.json(metadata);
-  console.log("metadataUploadResponse: ", metadataUploadResponse);
+async function retryUpload(uploadFunction, retries = 10, retryDelay = 5000) {
+    for (let attempt = 1; attempt <= retries; attempt++) {
+        try {
+            return await uploadFunction();
+        } catch (error) {
+            console.error(`    > Attempt to upload token metadata to IPFS ${attempt} failed:`, error.message);
 
-  // Return token metadata URL
-  return "ipfs://" + metadataUploadResponse.cid;
+            if (attempt < retries) {
+                console.log(`    > Retrying in ${retryDelay / 1000} seconds...`);
+                await new Promise((resolve) => setTimeout(resolve, retryDelay));
+            } else {
+                console.error("    > All attempts failed. Please check your network connection or try again later.");
+                throw error;
+            }
+        }
+    }
 }
 
 module.exports = { generateTokenUrl };
